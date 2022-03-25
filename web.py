ea92c6efbc7c541c0aa71b505eaddeec5d5f9381
kod30 <64425441+kod30@users.noreply.github.com>
2022-03-26 00:00:57 +0100

    Update web.py

--- web.py
+++ web.py
@@ -13,6 +13,8 @@ nltk.download('stopwords')
 nltk.download('punkt')
 import lxml
 
+st.markdown("[![Foo](https://www.dotcom-monitor.com/wp-content/uploads/datadog-logo.png)](https://www.dotcom-monitor.com/)")
+
 #country = st.sidebar.text_input('Country')
 country = st.selectbox('Select Country: ',('afghanistan','albania','andorra','angola','argentina','aruba','australia','belgium','brazil','canada','colombia','croatia','cuba','denmark','ecuador','france','germany','hong kong','hungary','italy','japan','spain','united states'))
 st.write('You selected:', country)
